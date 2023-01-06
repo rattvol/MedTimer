b3a3e14a6692f1a8f70bc5dab6afb7879e254867
Ratt <48017678+rattvol@users.noreply.github.com>
2023-01-06 17:31:05 +0300

    Add project files.

---
+++ MedTimer/MainActivity.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.Media;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using AndroidX.AppCompat.Widget;
+using AndroidX.AppCompat.App;
+using Google.Android.Material.FloatingActionButton;
+using Google.Android.Material.Snackbar;
+using Android.Widget;
+using Google.Android.Material.TextField;
+using Toolbar = AndroidX.AppCompat.Widget.Toolbar;
+using Android.Webkit;
+using Java.IO;
+using Stream = System.IO.Stream;
+
+namespace MedTimer
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        private TimerConfig _timerConfig = new TimerConfig();
+        private EditText _timerIntervalTextInputEditText;
+        private EditText _timerLoopCountTextInputEditText;
+        private TextView _timerOutputTextInputEditText;
+        private Button _timerRunButton;
+        private View root;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            Toolbar toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
+
+            //FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
+            //fab.Click += FabOnClick;
+
+
+            _timerIntervalTextInputEditText = FindViewById<EditText>(Resource.Id.timerInterval);
+            _timerLoopCountTextInputEditText = FindViewById<EditText>(Resource.Id.timerQuantity);
+            _timerOutputTextInputEditText = FindViewById<TextView>(Resource.Id.timerCounter);
+            _timerRunButton = FindViewById<Button>(Resource.Id.timerStart);
+            root = FindViewById<View>(Resource.Id.content);
+            _timerIntervalTextInputEditText.KeyPress += (object sender, View.KeyEventArgs e) =>
+            {
+                e.Handled = false;
+                if (e.Event.Action == KeyEventActions.Down && e.KeyCode == Keycode.Enter)
+                {
+                    _timerConfig.SetInterval(_timerIntervalTextInputEditText.Text);
+                    e.Handled = true;
+                }
+            };
+            _timerIntervalTextInputEditText.FocusChange += (object sender, View.FocusChangeEventArgs e) =>
+            {
+                if (!e.HasFocus)
+                {
+                    if (!String.IsNullOrEmpty(_timerIntervalTextInputEditText.Text))
+                        _timerConfig.SetInterval(_timerIntervalTextInputEditText.Text);
+                    else
+                        _timerIntervalTextInputEditText.Text = _timerConfig.Interval.ToString();
+                }
+                else
+                {
+                    _timerIntervalTextInputEditText.Text = string.Empty;
+                }
+            };
+
+            _timerLoopCountTextInputEditText.KeyPress += (object sender, View.KeyEventArgs e) =>
+            {
+                e.Handled = false;
+                if (e.Event.Action == KeyEventActions.Down && e.KeyCode == Keycode.Enter)
+                {
+                    _timerConfig.SetLoopCount(_timerLoopCountTextInputEditText.Text);
+                    e.Handled = false;
+                }
+            };
+
+            _timerLoopCountTextInputEditText.FocusChange += (object sender, View.FocusChangeEventArgs e) =>
+            {
+                if (!e.HasFocus)
+                {
+                    if (!String.IsNullOrEmpty(_timerLoopCountTextInputEditText.Text))
+                        _timerConfig.SetLoopCount(_timerLoopCountTextInputEditText.Text);
+                    else
+                        _timerLoopCountTextInputEditText.Text = _timerConfig.LoopCount.ToString();
+                }
+                else
+                {
+                    _timerLoopCountTextInputEditText.Text = string.Empty;
+                }
+            };
+
+            _timerRunButton.Click += TimerRunButtonOnClick;
+
+
+        }
+
+        /// <summary>
+        /// Метод подстановки значения таймера на интерфейс
+        /// </summary>
+        /// <param name="val"></param>
+        public void ShowTimer(string val)
+        {
+            _timerOutputTextInputEditText.Text = val;
+        }
+
+        /// <summary>
+        /// Обработка события нажатия на кнопку запуска таймера
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void TimerRunButtonOnClick(object sender, EventArgs e)
+        {
+            if (TimerCode.RunTimer(_timerConfig, ShowTimer))
+                _timerRunButton.Enabled = false;
+            Task.Factory.StartNew(() =>
+            {
+                do { }
+                while (!TimerCode.TimerTick(_timerConfig, ShowTimer, _timerOutputTextInputEditText.Text));
+                _timerRunButton.Enabled = true;
+            });
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.menu_main, menu);
+            return true;
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            int id = item.ItemId;
+            if (id == Resource.Id.action_settings)
+            {
+                return true;
+            }
+
+            return base.OnOptionsItemSelected(item);
+        }
+
+        private void FabOnClick(object sender, EventArgs eventArgs)
+        {
+            View view = (View)sender;
+            Snackbar.Make(view, "Replace with your own action", Snackbar.LengthLong)
+                .SetAction("Action", (View.IOnClickListener)null).Show();
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+
+}
---
+++ MedTimer/TimerCode.cs
@@ -0,0 +1,136 @@
+﻿using Android.Media;
+using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using Android.Widget;
+
+namespace MedTimer
+{
+    public delegate void OutPutFunction(string value);
+    
+    public static class TimerCode
+    {
+        private static bool _timerRunned =false;
+
+        private static MediaPlayer _mediaPlayer;
+
+        /// <summary>
+        /// Запуск таймера
+        /// </summary>
+        private static void runPlayer()
+        {
+            if (_mediaPlayer == null)
+            {
+                _mediaPlayer = new MediaPlayer();
+                _mediaPlayer.SetDataSource("https://www.soundjay.com/buttons/beep-01a.mp3");
+                _mediaPlayer.Prepare();
+                _mediaPlayer.SetVolume(100, 100);
+            }
+            _mediaPlayer.Start();
+        }
+
+        public static bool RunTimer(TimerConfig timerConfig, OutPutFunction outputFunc)
+        {
+            if (timerConfig == null) return false;
+            _timerRunned = true;
+            outputFunc(String.Format($"{timerConfig.LoopCount}:{timerConfig.Interval}"));
+            return true;
+        }
+
+        /// <summary>
+        /// Работа с запущенным таймером
+        /// </summary>
+        /// <param name="timerConfig"></param>
+        /// <param name="outputFunc"></param>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        public static bool TimerTick(TimerConfig timerConfig, OutPutFunction outputFunc, string text)
+        {
+            if (!_timerRunned) return true;
+            string[] values = text.Split(':');
+            if (values.Length == 2 && int.TryParse(values[1], out int counterInterval) && int.TryParse(values[0], out int counterLoop))
+            {
+                Task.Delay(1000).Wait();
+                int newCounterInterval = counterInterval - 1;
+                if (newCounterInterval > 0)
+                {
+                    outputFunc(String.Format($"{counterLoop}:{newCounterInterval}"));
+                }
+                else
+                {
+                    int newCounterLoop = counterLoop - 1;
+                    runPlayer();
+                    if (newCounterLoop > 0)
+                    {
+                        outputFunc(String.Format($"{newCounterLoop}:{timerConfig.Interval}"));
+                    }
+                    else
+                    {
+                        _timerRunned = false;
+                       outputFunc(String.Format($"{0}:{0}"));
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+    }
+
+    /// <summary>
+    /// Объект для хранения настроек таймера
+    /// </summary>
+    public class TimerConfig
+    {
+        private int _interval = 60;
+
+        public int Interval
+        {
+            get { return _interval; }
+            set
+            {
+                if (value < 1) value = 1;
+                _interval = value;
+            }
+        }
+
+        private int _loopCount =1;
+
+        public int LoopCount
+        {
+            get { return _loopCount; }
+            set
+            {
+                if (value < 1) value = 1;
+                _loopCount = value;
+            }
+        }
+
+        public void SetInterval(string interval)
+        {
+            if (int.TryParse(interval, out int intervalInt))
+            {
+                Interval = intervalInt;
+            }
+            else
+            {
+                Interval = 60;
+            }
+        }
+
+        public void SetLoopCount(string loopCount)
+        {
+            if (int.TryParse(loopCount, out int loopCountInt))
+            {
+                LoopCount = loopCountInt;
+            }
+            else
+            {
+                LoopCount = 1;
+            }
+        }
+
+    }
+}
